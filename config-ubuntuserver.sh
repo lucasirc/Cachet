73759898acca17def45dd2cd488614d0eacd7016
Lucas De Luca Castro <lucasirc@gmail.com>
2016-02-23 22:10:05 -0300

    Update config-ubuntuserver.sh
    install composer

--- config-ubuntuserver.sh
+++ config-ubuntuserver.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 sudo apt-get install php5 apache2 php5-mysql php5-gd
+
+# install composer
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
